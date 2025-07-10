c22db339213976f0f8edba3cd70728772a0d4538
juhui <juya202106@naver.com>
2025-07-11 00:29:29 +0900

    feat: 댓글/ 공감 시스템 구현 완료
    - 댓글 CRUD API 구현 (작성/조회/수정/삭제)
    - 비밀댓글 권한 체크 로직 구현
    - 댓글/게시글 공감 토글 기능 구현
    - DTO 패턴 적용으로 안전한 데이터 전송
    - MSA 환경 대응 Component Scan 설정
    - 소프트 삭제 및 JPA Auditing 적용

--- blog-service/src/main/java/com/playblog/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/playblog/blogservice/BlogServiceApplication.java
@@ -2,23 +2,26 @@ package com.playblog.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {
+        "com.playblog.blogservice.comment",
+        "com.playblog.blogservice.post",
+        "com.playblog.blogservice.common"
+})
 @EnableJpaAuditing
+@EntityScan(basePackages = {
+        "com.playblog.blogservice.comment.entity",
+        "com.playblog.blogservice.post.entity"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.playblog.blogservice.comment.repository",
+        "com.playblog.blogservice.post.repository"
+})
 public class BlogServiceApplication {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        // 토큰 설정이 없어서 일단 실행될 수 있게 security 비활성화
-        http.csrf(csrf -> csrf.disable())  // 새로운 문법
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        return http.build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlogServiceApplication.class, args);
--- blog-service/src/main/java/com/playblog/blogservice/comment/dto/CommentResponse.java
+++ blog-service/src/main/java/com/playblog/blogservice/comment/dto/CommentResponse.java
@@ -1,7 +1,6 @@
 package com.playblog.blogservice.comment.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
