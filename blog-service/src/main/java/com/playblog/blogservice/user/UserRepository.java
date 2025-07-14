87b22cea86b0d0ed44d3155aa818ec39ad9fd402
juhui <juya202106@naver.com>
2025-07-14 18:25:42 +0900

    refactor: UserRepository 위치 변경

--- blog-service/src/main/java/com/playblog/blogservice/user/UserRepository.java
+++ blog-service/src/main/java/com/playblog/blogservice/user/UserRepository.java
@@ -1,6 +1,5 @@
-package com.playblog.blogservice.common.repository;
+package com.playblog.blogservice.user;
 
-import com.playblog.blogservice.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
